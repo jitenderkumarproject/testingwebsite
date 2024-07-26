43003be8b8947720e385857b81de2be02fabb78d
jitenderkumarproject <jitenderkumarprojects@gmail.com>
2024-07-26 17:15:00 +0530

    Slider Compleated

---
+++ src/Caraousel.js
@@ -0,0 +1,55 @@
+import React from 'react'
+import { Carousel } from 'react-bootstrap';
+
+function Caraousel() {
+    return (
+        <Carousel>
+            <Carousel.Item>
+                <h5>First slide label</h5>
+                <img
+                    width="auto"
+                    height="auto"
+                    className="d-block w-100"
+                    src="assests/CarouselImage/1.png"
+                    alt="First slide"
+                />
+                <Carousel.Caption>
+                    <h5>First slide label</h5>
+                    <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <h5>First slide label</h5>
+                <img
+                   width="auto"
+                    height="auto"
+                    className="d-block w-100"
+                    src="assests/CarouselImage/2.png"
+                    alt="Second slide"
+                />
+                <Carousel.Caption>
+                    <h5>Second slide label</h5>
+                    <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+                </Carousel.Caption>
+            </Carousel.Item>
+            <Carousel.Item>
+                <h5>First slide label</h5>
+                <img
+                    width="auto"
+                    height="auto"
+                    className="d-block w-100"
+                    src="assests/CarouselImage/3.png"
+                    alt="Third slide"
+                />
+                <Carousel.Caption>
+                    <h5>Third slide label</h5>
+                    <p>
+                        Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+                    </p>
+                </Carousel.Caption>
+            </Carousel.Item>
+        </Carousel>
+    );
+}
+
+export default Caraousel
